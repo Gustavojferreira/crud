82e0f4b775eef2ab249136526fdd9a5cc0eea126
Gustavo F. <ferreiragust2002@gmail.com>
2024-09-03 14:44:30 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Crud-python-com-Flask
+ Crud para aprender a utilizar python e o flask
---
+++ app/routes.py
@@ -0,0 +1,88 @@
+from app import app
+from flask import render_template, request, redirect, url_for
+import mysql.connector
+
+# conectar BD
+def dbconnect():
+    return mysql.connector.connect(
+    host= 'localhost',
+    user= 'root',
+    password= '1234', 
+    database='pycrud',
+
+    )
+
+@app.route('/')
+@app.route('/home')
+def home():
+   
+    return render_template('home.html')
+
+# CREATE    
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        nome_produto = request.form['nome_produto']
+        valor_produto = request.form['valor_produto']
+
+        conn = dbconnect()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO vendas (nome_produto, valor_produto) VALUES (%s, %s)", (nome_produto, valor_produto))
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        return redirect('home')
+
+    return render_template('create.html')
+
+# READ
+@app.route('/recovery')
+def recovery():
+    conn = dbconnect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM vendas")
+    produtos = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    return render_template('recovery.html', produtos=produtos)
+
+# Update 
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+    if request.method == 'POST':
+        idvendas = request.form['id']
+        nome_produto = request.form['nome_produto']
+        valor_produto = request.form['valor_produto']
+
+        conn = dbconnect()
+        cursor = conn.cursor()
+        cursor.execute("UPDATE vendas SET nome_produto=%s, valor_produto=%s WHERE idvendas=%s", (nome_produto, valor_produto, idvendas))
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        return redirect('home')
+
+    return render_template('update.html')
+
+# Delete 
+@app.route('/delete', methods=['GET', 'POST'])
+def delete():
+    if request.method == 'POST':
+        idvendas = request.form['id']
+
+        conn = dbconnect()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM vendas WHERE idvendas=%s", [idvendas])
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        return redirect('home')
+    else:
+        return render_template('delete.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
